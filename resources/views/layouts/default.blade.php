0e941bfb42ad3fa847c9b986c5dd6f66680f8fe8
KevinGao <wogaoxiaojun@126.com>
2018-09-14 16:54:34 +0800

    2018091416

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -19,9 +19,9 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">learing</a>
-                </li>
+                {{--<li class="nav-item active">--}}
+                    {{--<a class="nav-link" href="#">learing</a>--}}
+                {{--</li>--}}
                 <li class="nav-item active">
                     <a class="nav-link" href="{{route('user.index')}}">用户列表</a>
                 </li>
